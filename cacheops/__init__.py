f5e02889b328b2f42fc09742528a1c9dcb99186e
Pratik <pratik@triotech.co.in>
2024-12-24 11:17:46 +0530

    Reuse existing cacheops config

--- cacheops/__init__.py
+++ cacheops/__init__.py
@@ -1,19 +1,13 @@
 __version__ = '7.1'
 VERSION = tuple(map(int, __version__.split('.')))
 
-from django.apps import AppConfig
+import django
 from .simple import *  # noqa
 from .query import *  # noqa
 from .invalidation import *  # noqa
 from .reaper import *  # noqa
 from .templatetags.cacheops import *  # noqa
-from .transaction import *  # noqa
 
-class CacheopsConfig(AppConfig):
-    name = 'cacheops'
 
-    def ready(self):
-        install_cacheops()
-        install_cacheops_transaction_support()
-
-default_app_config = 'cacheops.CacheopsConfig'
+if django.VERSION < (3, 2):
+    default_app_config = "cacheops.apps.CacheopsConfig"
